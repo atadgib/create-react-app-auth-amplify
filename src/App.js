4bc137d3a8b37ba24cc9626d215ef8b97165131d
atadgib <natarajan.ramachandran@gmail.com>
2021-05-16 12:29:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,22 +9,15 @@ Amplify.configure(aws_exports);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            You let the dogs out!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React man!
-          </a>
-        </header>
-      </div>
+      function tick() {
+        const element = (
+          <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+          </div>
+        );
+        ReactDOM.render(element, document.getElementById('root'));
+      }
     );
   }
 }
