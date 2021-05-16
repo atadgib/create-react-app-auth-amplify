78fbc8fe172470cc7c864da4aad6102163761e76
atadgib <natarajan.ramachandran@gmail.com>
2021-05-16 13:00:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
 class App extends Component {
   render() {
-    return (
-      function tick() {
-        const element = (
-          <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-          </div>
-        );
-        ReactDOM.render(element, document.getElementById('root'));
-      }
-    );
+    return ( setInterval(tick,1000)    );
   }
 }
 
